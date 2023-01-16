058737b22f0c85ced9bb43a21f037a3fb8c9eaff
Shreehari C R <shreehari000@gamil.com>
2023-01-16 17:36:42 +0530

    completed first assignment with executed 3 callback functions asynchronously

--- Week-4/problem3.js
+++ Week-4/problem3.js
@@ -1,20 +1,30 @@
-const fibSeries = (num) => ({
-    [Symbol.iterator]: () => {
-        let i = 1;
-        let old = 0;
-        let latest = 0;
-        return {
-            next: () => {
-                if(i++ <= num) {
-                    [old, latest] = [latest, (old + latest) || 1];
-                    return { value: old, done: false }
-                }
-                else {
-                    return { done: true }
-                } 
-            }
-        }
-    }
-});
+// const fibSeries = (num) => ({
+//     [Symbol.iterator]: () => {
+//         let i = 1;
+//         let old = 0;
+//         let latest = 0;
+//         return {
+//             next: () => {
+//                 if(i++ <= num) {
+//                     [old, latest] = [latest, (old + latest) || 1];
+//                     return { value: old, done: false }
+//                 }
+//                 else {
+//                     return { done: true }
+//                 } 
+//             }
+//         }
+//     }
+// });
 
-console.log([...fibSeries(6)]);
+// console.log([...fibSeries(6)]);
+
+class Sample {
+    arr = [34, 4]
+    static [Symbol.hasInstance] () {
+        return 0;
+    }
+}
+
+const samp = new Sample();
+console.log(samp instanceof Sample);
