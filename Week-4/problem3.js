282d118f6088e6fef4e2fa8363ac1ce9c1264c0c
Shreehari C R <shreehari000@gamil.com>
2023-01-21 12:46:08 +0530

    used for of loop for iteration of fibSeries

--- Week-4/problem3.js
+++ Week-4/problem3.js
@@ -1,30 +1,22 @@
-// const fibSeries = (num) => ({
-//     [Symbol.iterator]: () => {
-//         let i = 1;
-//         let old = 0;
-//         let latest = 0;
-//         return {
-//             next: () => {
-//                 if(i++ <= num) {
-//                     [old, latest] = [latest, (old + latest) || 1];
-//                     return { value: old, done: false }
-//                 }
-//                 else {
-//                     return { done: true }
-//                 } 
-//             }
-//         }
-//     }
-// });
-
-// console.log([...fibSeries(6)]);
-
-class Sample {
-    arr = [34, 4]
-    static [Symbol.hasInstance] () {
-        return 0;
+const fibSeries = (num) => ({
+    [Symbol.iterator]: () => {
+        let i = 1;
+        let old = 0;
+        let latest = 0;
+        return {
+            next: () => {
+                if(i++ <= num) {
+                    [old, latest] = [latest, (old + latest) || 1];
+                    return { value: old, done: false }
+                }
+                else {
+                    return { done: true }
+                } 
+            }
+        }
     }
-}
+});
 
-const samp = new Sample();
-console.log(samp instanceof Sample);
+for (const num of fibSeries(6)) console.log(num);
+
+
